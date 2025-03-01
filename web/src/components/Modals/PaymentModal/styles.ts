acf33e4b404396e2ac5910330d0f397c500cd403
Robby Evans <ruttoevans001@gmail.com>
2025-03-01 20:54:19 +0300

    minor styling fix

--- web/src/components/Modals/PaymentModal/styles.ts
+++ web/src/components/Modals/PaymentModal/styles.ts
@@ -32,7 +32,7 @@ export const ModalContent = styled.div`
   @media (max-width: 480px) {
     /* Reduce padding and adjust width for smaller screens */
     padding: ${spacing.lg};
-    max-height: 70vh;
+    max-height: 80vh;
     margin: 60px 16px 0 16px;
   }
 `;
