3c5d9cb683edca68bf64e3241a9eb1b734313cb9
Robby Evans <ruttoevans001@gmail.com>
2025-03-30 01:18:24 +0300

    minor fix

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -4,24 +4,27 @@ import { useNavigate } from "react-router-dom";
 import * as S from "./styles";
 import Search from "../Search/Search";
 import NotificationContainer from "../../containers/NotificationContainer";
+import { useAdmins } from "../../redux/hooks/useAdmin";
 
 const Navbar: React.FC = () => {
   const [showSearch, setShowSearch] = useState(false);
-
   const navigate = useNavigate();
-
+  const { isAuthenticated } = useAdmins();
   const toggleSearch = () => setShowSearch((prev) => !prev);
 
   return (
     <S.Navbar>
       <S.AppName onClick={() => navigate("/dashboard")}>KODI</S.AppName>
-
       <S.NavActions>
-        <Search isVisible={showSearch} />
-        <S.IconButton onClick={toggleSearch} aria-label="Search">
-          <FaSearch />
-        </S.IconButton>
-        <NotificationContainer />
+        {isAuthenticated && (
+          <>
+            <Search isVisible={showSearch} />
+            <S.IconButton onClick={toggleSearch} aria-label="Search">
+              <FaSearch />
+            </S.IconButton>
+            <NotificationContainer />
+          </>
+        )}
       </S.NavActions>
     </S.Navbar>
   );
