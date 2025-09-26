7ffad33625cffa7a2529a506956ea0a891949bf4
Robby Evans <ruttoevans001@gmail.com>
2025-09-26 20:54:35 +0300

    minor fix

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,9 +1,13 @@
+# Uses comma-separated origins from ENV to keep config flexible per environment.
+
+allowed_origins = ENV.fetch("CORS_ALLOWED_ORIGINS", "").split(/\s*,\s*/).reject(&:empty?)
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_URL", "http://localhost:5173").split(",")
-    resource '*',
-             headers: :any,
-             methods: [:get, :post, :patch, :put, :delete, :options],
-             credentials: true
+    origins(*allowed_origins)
+    resource "*",
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      credentials: true
   end
 end
