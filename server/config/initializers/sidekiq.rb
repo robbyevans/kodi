859f4a74eeba868a84aecb4813148023b1ec881b
Robby Evans <ruttoevans001@gmail.com>
2025-09-27 10:07:52 +0300

    minor fix

--- server/config/initializers/sidekiq.rb
+++ server/config/initializers/sidekiq.rb
@@ -1,18 +1,23 @@
-require 'sidekiq'
-require 'sidekiq-scheduler'
-require 'yaml'
-require 'erb'
+require "sidekiq"
+require "sidekiq-scheduler"
+require "yaml"
+require "erb"
+
+def sidekiq_redis_url
+  ENV["SIDEKIQ_REDIS_URL"].presence ||
+    ENV["REDIS_URL"].presence ||
+    ENV["REDIS_TLS_URL"].presence || # some stacks use this
+    "redis://localhost:6379/0"
+end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = { url: sidekiq_redis_url }
 
-  schedule_file = Rails.root.join('config', 'schedule', "#{Rails.env}.yml")
-
+  schedule_file = Rails.root.join("config", "schedule", "#{Rails.env}.yml")
   if File.exist?(schedule_file)
     config.on(:startup) do
       raw_yaml = ERB.new(File.read(schedule_file)).result
-      schedule = Psych.safe_load(raw_yaml, aliases: true)
-
+      schedule  = Psych.safe_load(raw_yaml, aliases: true) || {}
       Sidekiq::Scheduler.dynamic = true
       Sidekiq.schedule = schedule
       Sidekiq::Scheduler.reload_schedule!
@@ -23,5 +28,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = { url: sidekiq_redis_url }
 end
